7123bb7524b7b62cf717f1c6dce0c18135d74c87
dooohun <ehgns0305@naver.com>
2026-03-09 16:28:04 +0900

    Initial Commit

---
+++ IndoorNavigation-iOSTests/IndoorNavigation_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  IndoorNavigation_iOSTests.swift
+//  IndoorNavigation-iOSTests
+//
+//  Created by 김도훈 on 3/9/26.
+//
+
+import Testing
+@testable import IndoorNavigation_iOS
+
+struct IndoorNavigation_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
